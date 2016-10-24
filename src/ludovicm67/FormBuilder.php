c11c9f977ff768e0a6777c51f6b4b4d220fc8c3a
ludovicm67 <ludovicmuller1@gmail.com>
2016-10-24 22:46:34 +0200

    update version number

--- src/ludovicm67/FormBuilder.php
+++ src/ludovicm67/FormBuilder.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     FormBuilder class
- * @version     1.0.0
+ * @version     1.0.2
  * @author      ludovicm67 <ludovicmuller1@gmail.com>
  * @license     This software is licensed under the MIT license: http://opensource.org/licenses/MIT
  *
